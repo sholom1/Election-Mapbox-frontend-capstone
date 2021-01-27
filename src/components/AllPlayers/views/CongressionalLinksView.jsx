fb66648fbb1421e60733ea2c821a1798960873e9
Adina Scheinfeld <Adina.Scheinfeld20@bcmail.cuny.edu>
2021-01-27 18:46:13 -0500

    added navigation bar

---
+++ src/components/AllPlayers/views/CongressionalLinksView.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles/AllPlayersView.css';
+import Navbar from '../navigation/Navbar';
+// import PropTypes from 'prop-types';
+
+const CongressionalLinksView = () => {
+  return (
+    <div className="congressional">
+      <Navbar />
+      <h1>View Congressional Elections</h1>
+      <h2>Links go here</h2>
+    </div>
+  );
+};
+
+
+export default CongressionalLinksView;
