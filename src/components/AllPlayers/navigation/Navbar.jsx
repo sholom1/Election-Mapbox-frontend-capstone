a13b68b53f411b3affa5ad96879a0e36cd1d9fbd
Adina Scheinfeld <Adina.Scheinfeld20@bcmail.cuny.edu>
2021-01-31 11:58:56 -0500

    fixed navbar styling

--- src/components/AllPlayers/navigation/Navbar.jsx
+++ src/components/AllPlayers/navigation/Navbar.jsx
@@ -6,13 +6,17 @@ class Navbar extends Component {
    render() { 
         return ( 
             <div className='container'>
-                <Link className='link' to='/'>Home</Link>
-                <Link className='link' to='/login'>Log In</Link>
-                <Link className='link' to='/signup'>Sign Up</Link>
-                <Link className='link' to='/upload'>Upload</Link>
-                <Link className='link' to='/categories/citywide'>Citywide Elections</Link>
-                <Link className='link' to='/categories/citycouncil'>City Council Elections</Link>
-                <Link className='link' to='/categories/congressional'>Congressional Elections</Link>
+                    <div className='rightContainer'>
+                    <Link className='link' to='/'>Home</Link>
+                    <Link className='link' to='/categories/citywide'>Citywide Elections</Link>
+                    <Link className='link' to='/categories/citycouncil'>City Council Elections</Link>
+                    <Link className='link' to='/categories/congressional'>Congressional Elections</Link>
+                    <Link className='link' to='/upload'>Upload</Link>               
+                </div>
+                <div className='rightContainer'>
+                    <Link className='link' to='/login'>Log In</Link>
+                    <Link className='link' to='/signup'>Sign Up</Link>
+                </div>
             </div>
          );
     }
